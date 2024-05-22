8757869e66c4f47d689c43d1a38e52dc7ab6a760
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-05-22 13:32:10 +0700

    finish question 1-5

--- question-4.js
+++ question-4.js
@@ -5,3 +5,7 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let lowestInventory = inventory.sort((a, b) => a.quantity - b.quantity)[0];
+console.log(
+  `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${lowestInventory.name} ซึ่งมี ${lowestInventory.quantity} ชิ้น`
+);
